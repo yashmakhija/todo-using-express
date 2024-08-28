d79d68381ff5f36442c387cde829d884ac002784
yashmakhija <yashkrmakhija@gmail.com>
2024-08-28 16:24:38 +0530

    added package-loc.json

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+require('dotenv').config()
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend")));
 
