b414436ff60552eb969f78ac69a065365d0b25f1
yashmakhija <yashkrmakhija@gmail.com>
2024-08-28 23:41:47 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const path = require("path");
 
 require('dotenv').config()
 
 app.use(express.json());
+app.use(cors())
 app.use(express.static(path.join(__dirname, "../frontend")));
 
 let todos = [];
